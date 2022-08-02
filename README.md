4315917450e36d1999aa3021d574d21a2415c354
YvonneD <diyifang@ymail.com>
2022-08-02 10:11:45 -0400

    v0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # responsive-admin-dashboard
+
 html,css,js
---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+  {
+    productName: "1",
+    productNumber: "222",
+    paymentStatus: "due",
+    shipping: "pending",
+  },
+];
